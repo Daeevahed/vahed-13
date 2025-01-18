8e4988a9b9518946354cf4a77f6fccbcce8dec09
vahed <vahed.eghdami@gmail.com>
2025-01-19 02:41:45 +0330

    start of project

---
+++ src/main/java/vahed/vahed_13/Vahed13Application.java
@@ -0,0 +1,13 @@
+package vahed.vahed_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vahed13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vahed13Application.class, args);
+	}
+
+}
---
+++ src/test/java/vahed/vahed_13/Vahed13ApplicationTests.java
@@ -0,0 +1,13 @@
+package vahed.vahed_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vahed13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
